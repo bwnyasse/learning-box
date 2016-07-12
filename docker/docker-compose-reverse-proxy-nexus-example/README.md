ef795844b947030e89e002c415961a3f014d73ca
bwnyasse <risbonyasse@gmail.com>
2016-07-12 22:53:01 +0200

    Addind reverse proxy nginx over nexus example

---
+++ docker/docker-compose-reverse-proxy-nexus-example/README.md
@@ -0,0 +1,3 @@
+# Reverse Proxy Example with Nexus as Service
+
+[Official doc](https://books.sonatype.com/nexus-book/reference/install-sect-proxy.html)
--- docker/nexus/customization/upload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --upload-file artifacts.zip -u admin:admin123 -v 127.0.0.1:8081/nexus/service/local/repositories/releases/content-compressed
