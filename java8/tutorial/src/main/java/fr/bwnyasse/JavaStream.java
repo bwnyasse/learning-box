2062a6fd299a1724f977bc4fda29d17d1b0b2846
bwnyasse <risbonyasse@gmail.com>
2019-01-01 19:40:28 +0100

    Java8: parallelStream

--- java8/tutorial/src/main/java/fr/bwnyasse/JavaStream.java
+++ java8/tutorial/src/main/java/fr/bwnyasse/JavaStream.java
@@ -135,4 +135,14 @@ public class JavaStream {
 
   }
 
+  private static void parallemStreamExample() {
+    List<Integer> list = new ArrayList<Integer>();
+    for (int i = 1; i < 10; i++) {
+      list.add(i);
+    }
+    // Here creating a parallel stream
+    Stream<Integer> stream = list.parallelStream();
+    Integer[] evenNumbersArr = stream.filter(i -> i % 2 == 0).toArray(Integer[]::new);
+    System.out.print(evenNumbersArr);
+  }
 }
