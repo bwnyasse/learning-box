4412548824b7249a771316f800aa06147d869795
bwnyasse <risbonyasse@gmail.com>
2020-05-08 20:59:50 +0000

    feat(PCEP): Using Python Conditionals

---
+++ python/PCEP/fizz-buzz-item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.7
+
+value = int(input("Enter an integer value:  "))
+
+if value % 5 == 0 and value % 3 == 0:
+    print("FizzBuzz")
+elif value % 3 == 0:
+    print("Fizz")
+elif value % 5 == 0:
+    print("Buzz")
+else:
+    print(value)
