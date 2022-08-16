7a383e1f67ee7968a977e3df0f2ea940888ea947
bwnyasse <5323628+bwnyasse@users.noreply.github.com>
2022-08-16 11:27:06 -0400

    feat(svelte.js: setup udemy complete guide

---
+++ svelte.js/udemy-complete-guide/first-app/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'Max';
+	let age = 30;
+
+	function incrementAge() {
+		age += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my age is {age}!</h1>
+<button on:click="{incrementAge}">Change Age</button>
