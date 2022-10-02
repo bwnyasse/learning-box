2eca9e4e04d8d76673154af957c38a3439eae9fe
bwnyasse <5323628+bwnyasse@users.noreply.github.com>
2022-10-01 22:32:35 -0400

    feat(svelte.js):Binding to Element Properties

---
+++ svelte.js/udemy-complete-guide/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  let name = "Max";
+  let age = 30;
+
+  // let uppercaseName; not required!
+
+  $: uppercaseName = name.toUpperCase();
+
+  $: console.log(name);
+
+  $: if (name === "Maximilian") {
+    console.log("It runs!");
+    age = 31;
+  }
+
+  function incrementAge() {
+    age += 1;
+  }
+
+  function changeName() {
+    name = "Maximilian";
+  }
+
+  function nameInput(event) {
+    const enteredValue = event.target.value;
+    name = enteredValue;
+  }
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+<!-- <button on:click="{changeName}">Change Name</button> -->
+<input type="text" value={name} on:input={nameInput} />
