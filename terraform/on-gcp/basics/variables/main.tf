06da24f1ff5568baaa710a3dd298ae27e5403b32
bwnyasse <5323628+bwnyasse@users.noreply.github.com>
2023-01-02 13:30:09 -0500

    feat(terraform): variables

---
+++ terraform/on-gcp/basics/variables/main.tf
@@ -0,0 +1,4 @@
+resource local_file sample_res {
+  filename = var.filename1
+  content = var.content1
+}
---
+++ terraform/on-gcp/basics/variables/variables.tf
@@ -0,0 +1,9 @@
+variable filename1 {
+  type        = string
+  default     = "sample_on_variable.txt"
+}
+
+variable content1 {
+  type        = string
+  default     = "I am loving Terraform from variables"
+}
