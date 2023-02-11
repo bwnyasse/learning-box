08832bb64bc35a7dfd0242823a0992ee6986280d
bwnyasse <5323628+bwnyasse@users.noreply.github.com>
2023-02-10 23:49:20 -0500

    feat(flutter-remote-config): refactoring

---
+++ dart/flutter/flutter_moviesapp_demo_firebase_remote_config/lib/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_moviesapp_demo_firebase_remote_config/screens/dashboard/components/my_fields.dart';
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+import 'components/header.dart';
+
+import 'components/recent_files.dart';
+import 'components/storage_details.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        primary: false,
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [
+            const Header(),
+            const SizedBox(height: defaultPadding),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Column(
+                    children: [
+                      const MyFiles(),
+                      const SizedBox(height: defaultPadding),
+                      const RecentFiles(),
+                    ],
+                  ),
+                ),
+                  const SizedBox(width: defaultPadding),
+                  const Expanded(
+                    flex: 2,
+                    child: StorageDetails(),
+                  ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
