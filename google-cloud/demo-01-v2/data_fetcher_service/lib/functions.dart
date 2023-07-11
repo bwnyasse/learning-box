098ed72fecccd286e11344d2c5fd351004e72a6f
bwnyasse <5323628+bwnyasse@users.noreply.github.com>
2023-07-11 08:10:07 -0400

    gcloud(demo-1-v2): step 5

--- google-cloud/demo-01-v2/data_fetcher_service/lib/functions.dart
+++ google-cloud/demo-01-v2/data_fetcher_service/lib/functions.dart
@@ -12,5 +12,28 @@ export 'models/models.dart';
 
 @CloudFunction()
 Future<Response> function(Request request) async {
+  try {
+    // LOAD STOCK CONFIGURATION
+    List<StockConfig> stocks = await service.loadStockConfiguration();
+
+    List<Future<void>> apiCalls = stocks.map((element) async {
+      // FETCH STOCK QUOTE
+      StockQuoteResponse response =
+          await service.fetchStockQuote(element.stock, element.exchange);
+
+      final jsonToStore = mergeJson(response.toJson(), element.toJson());
+
+      // WRITE TO STORAGE
+      await service.writeToStorage(element.stock, jsonEncode(jsonToStore));
+      print(
+          'Context - symbol : ${element.stock} - Response: ${jsonToStore['lastPrice']}');
+    }).toList();
+
+    await Future.wait(apiCalls);
+  } catch (error, stackTrace) {
+    // Handle or report the error accordingly.
+    print('An error occurred: $error\n$stackTrace');
+  }
+
   return Response.ok('Cloud run with Dart Lang is OK !');
 }
